145fda23d01dc3aca1cfd58c85f3d52e04552366
Jeffrey Turns <51068771+jeffreyturns@users.noreply.github.com>
2024-03-13 05:52:28 +0200

    fix: remove SSR of SEO

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,13 +3,6 @@ export default defineNuxtConfig({
   ssr: false,
   devtools: { enabled: true },
   modules: ['@nuxt/ui'],
-  app: {
-    head: {
-      htmlAttrs: { class: 'ui-scrollbars' },
-      meta: [
-        // { 'http-equiv': 'content-security-policy', content: 'default-src &#39;self&#39;; script-src &#39;self&#39; &#39;unsafe-inline&#39; &#39;unsafe-eval&#39;; style-src &#39;self&#39; &#39;unsafe-inline&#39;;' }
-      ]
-    }
-  },
+  app: { head: { htmlAttrs: { class: 'ui-scrollbars' } } },
   css: ['~/styles/base.css']
 })
