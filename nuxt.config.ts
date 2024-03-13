ce81d9f9e68f04a935937e28b4ebe6e69a523e32
Jeffrey Turns <51068771+jeffreyturns@users.noreply.github.com>
2024-03-13 05:54:47 +0200

    chore: i forgor ssr false

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  ssr: false,
+  ssr: true,
   devtools: { enabled: true },
   modules: ['@nuxt/ui'],
   app: { head: { htmlAttrs: { class: 'ui-scrollbars' } } },
