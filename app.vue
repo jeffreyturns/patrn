cef46e77bc2afcedb680382e052b677bbe03765e
Jeffrey Turns <51068771+jeffreyturns@users.noreply.github.com>
2024-03-13 05:16:41 +0200

    revert: to prev commit

--- app.vue
+++ app.vue
@@ -1,23 +1,25 @@
-<!-- eslint-disable @stylistic/max-len -->
 <script setup lang="ts">
 useHead({
   meta: [
-    { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-    { property: 'title', content: 'Patrn - create beautiful patterns with mathematics' },
-    { name: 'description', content: 'Discover the magic of transforming mathematical formulas into stunning visual patterns with Patrn. Explore the intersection of art and mathematics, where sophistication meets simplicity.' },
-    { property: 'og:title', content: 'Patrn - create beautiful patterns with mathematics' },
-    { property: 'og:url', content: 'https://patrn.vercel.app' },
-    { property: 'og:description', content: 'Discover the magic of transforming mathematical formulas into stunning visual patterns with Patrn. Explore the intersection of art and mathematics, where sophistication meets simplicity.' },
-    { property: 'og:image', content: 'https://patrn.vercel.app/social-card.png' },
-    { name: 'twitter:title', content: 'Patrn - create beautiful patterns with mathematics' },
-    { name: 'twitter:description', content: 'Discover the magic of transforming mathematical formulas into stunning visual patterns with Patrn. Explore the intersection of art and mathematics, where sophistication meets simplicity.' },
-    { name: 'twitter:image', content: 'https://patrn.vercel.app/social-card.png' },
-    { name: 'twitter:card', content: 'summary_large_image' }
+    { name: 'viewport', content: 'width=device-width, initial-scale=1' }
   ],
   link: [
     { rel: 'icon', href: '/favicon.ico' }
   ],
   htmlAttrs: { lang: 'en' }
+})
+
+useSeoMeta({
+  title: 'Patrn - create beautiful patterns with mathematics',
+  ogTitle: 'Patrn - create beautiful patterns with mathematics',
+  ogUrl: 'https://patrn.vercel.app',
+  description: 'Discover the magic of transforming mathematical formulas into stunning visual patterns with Patrn. Explore the intersection of art and mathematics, where sophistication meets simplicity.',
+  ogDescription: 'Discover the magic of transforming mathematical formulas into stunning visual patterns with Patrn. Explore the intersection of art and mathematics, where sophistication meets simplicity.',
+  ogImage: 'https://patrn.vercel.app/social-card.png',
+  twitterTitle: '[twitter:title]',
+  twitterDescription: '[twitter:description]',
+  twitterImage: 'https://patrn.vercel.app/social-card.png',
+  twitterCard: 'summary_large_image'
 })
 </script>
 
